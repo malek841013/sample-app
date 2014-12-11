c87d4a42cc4c6d8be52276aa1b79ce589c1be6c9
Tomas Malek <malek841013@hotmail.com>
2014-12-11 05:48:06 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'static_pages#home'
 
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get 'help'    =>  'static_pages#help'
+  get 'about'   =>  'static_pages#about'
+  get 'contact' =>  'static_pages#contact'
+  get 'signup'  =>  'users#new'
 
   # root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
